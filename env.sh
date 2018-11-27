1c428687773f5564c1da2c713f6369e736fb81de
Maksim Rakitin <maxim.rakitin@gmail.com>
2018-11-26 20:14:30 -0500

    Cleaned setup-olog.sh to use for building a new image with olog available

--- env.sh
+++ env.sh
@@ -1,5 +1,6 @@
 export ADMIN_PASSWORD=password
 export CERTIFICATE_PASSWORD=password
+export USER_PASSWORD=1234
 export DB_USER=olog_user
 export DB_PASSWORD=password
 export DB_URL=olog-mysql-db
--- setup-olog.sh
+++ setup-olog.sh
@@ -6,7 +6,6 @@ set -e
 
 export PATH=${PATH}:${GLASSFISH_HOME}/bin
 
-POSTGRES_DATASOURCE=org.postgresql.ds.PGConnectionPoolDataSource
 MYSQL_DATASOURCE=com.mysql.jdbc.jdbc2.optional.MysqlConnectionPoolDataSource
 
 RESOURCE_TYPE=javax.sql.ConnectionPoolDataSource
@@ -14,57 +13,29 @@ CONNECTION_POOL_NAME=OlogPool
 
 # Database's environment variables
 DB_URL=${DB_URL:-olog-mysql-db}
-
-DB_POSTGRES_URL=jdbc:postgresql://${DB_URL}:5432/olog
 DB_MYSQL_URL=${DB_URL}
-
 DB_USER=${DB_USER:-olog_user}
 DB_PASSWORD=${DB_PASSWORD:-password}
 DB_NAME=${DB_NAME:-olog}
-
-# LDAP server's environment server
-
-REALM_CLASS_NAME=com.sun.enterprise.security.auth.realm.ldap.LDAPRealm
-REALM_JAAS_CTX=ldapRealm
-
-# Controls group test LDAP server
-# REALM_BASE_DN="\"cn=users,dc=lnls,dc=br\""
-# REALM_GROUP_DN="\"cn=olog-admin,dc=lnls,dc=br\""
-# REALM_URL="\"ldap://10.0.4.57:389\""
-# REALM_SEARCH_FILTER="\"uid=%s\""
-# REALM_GROUP_FILTER="\"memberuid=%s\""
-
-# CNPEM
-# REALM_BASE_DN="\"OU=Users,OU=LNLS,DC=abtlus,DC=org,DC=br\""
-# REALM_URL="\"ldap://ad1.abtlus.org.br:389\""
-# REALM_SEARCH_FILTER="\"sAMAccountName=%s\""
-# REALM_SEARCH_BIND_DN="\"${BIND_DN}\""
-# REALM_SEARCH_BIND_PASS="\"${BIND_PASS}\""
-
-# JNDI_RESOURCE_TYPE="javax.naming.directory.Directory"
-# JNDI_FACTORY_CLASS="com.sun.jndi.ldap.LdapCtxFactory"
-# JNDI_URL="\"ldap://localhost:389/dc=cf-test,dc=bnl,dc=gov\""
-# JNDI_PRINCIPAL="\"dc=cf-test,dc=bnl,dc=gov\""
 
 # File-based authentication. See
 # https://github.com/lnls-sirius/docker-olog-server/issues/2 for more details.
 REALM_CLASS_NAME=com.sun.enterprise.security.auth.realm.file.FileRealm
 REALM_PROPERTY=jaas-context=fileRealm:defaultuser=admin:file=\${com.sun.aas.instanceRoot}/config/keyfile
 
-
-echo "AS_ADMIN_PASSWORD=" > /tmp/glassfishpwd
-echo "AS_ADMIN_NEWPASSWORD=${ADMIN_PASSWORD}" >> /tmp/glassfishpwd
-echo "AS_ADMIN_MASTERPASSWORD=changeit" >> /tmp/glassfishpwd
+echo "AS_ADMIN_PASSWORD="                                  > /tmp/glassfishpwd
+echo "AS_ADMIN_NEWPASSWORD=${ADMIN_PASSWORD}"             >> /tmp/glassfishpwd
+echo "AS_ADMIN_MASTERPASSWORD=changeit"                   >> /tmp/glassfishpwd
 echo "AS_ADMIN_NEWMASTERPASSWORD=${CERTIFICATE_PASSWORD}" >> /tmp/glassfishpwd
 
 asadmin change-master-password --passwordfile=/tmp/glassfishpwd domain1
 asadmin --user=admin --passwordfile=/tmp/glassfishpwd change-admin-password --domain_name domain1
 
-echo "AS_ADMIN_PASSWORD=${ADMIN_PASSWORD}" > /tmp/glassfishpwd
-echo "AS_ADMIN_MASTERPASSWORD=${CERTIFICATE_PASSWORD}" >> /tmp/glassfishpwd
+echo "AS_ADMIN_PASSWORD=${ADMIN_PASSWORD}"                 > /tmp/glassfishpwd
+echo "AS_ADMIN_MASTERPASSWORD=${CERTIFICATE_PASSWORD}"    >> /tmp/glassfishpwd
 
 # Start asadmin console and the domain
-asadmin --user=admin --passwordfile=/tmp/glassfishpwd  start-domain
+asadmin --user=admin --passwordfile=/tmp/glassfishpwd start-domain
 
 asadmin --user=admin --passwordfile=/tmp/glassfishpwd --host localhost --port 4848 enable-secure-admin
 
@@ -76,28 +47,7 @@ asadmin --user=admin --passwordfile=/tmp/glassfishpwd restart-domain
 # Grant derby socket permissions and starts derby connection pool
 asadmin --user=admin --passwordfile=/tmp/glassfishpwd start-database
 
-#### POSTGRES
-# Configures connection pool
-# asadmin --user=admin --passwordfile=/tmp/glassfishpwd \
-#                 create-jdbc-connection-pool \
-#                 --datasourceclassname ${POSTGRES_DATASOURCE} \
-#                 --restype ${RESOURCE_TYPE} \
-#                 --property User=${DB_USER}:Password=${DB_PASSWORD}:Url=\"${DB_POSTGRES_URL}\":DatabaseName=${DB_NAME} \
-#                 ${CONNECTION_POOL_NAME}
-#
-# # Configures connection resource
-# asadmin --user=admin --passwordfile=/tmp/glassfishpwd \
-#                 create-jdbc-resource \
-#                 --connectionpoolid ${CONNECTION_POOL_NAME} \
-#                 jdbc/olog
-
-#### MYSQL
-# Configures connection pool
-
-# Waits for the database to be ready
-chmod +x /opt/wait-for-it/wait-for-it.sh
-/opt/wait-for-it/wait-for-it.sh ${DB_MYSQL_URL}:3306
-
+# Configures connection pool for MySQL
 asadmin --user=admin --passwordfile=/tmp/glassfishpwd \
                create-jdbc-connection-pool \
                --datasourceclassname ${MYSQL_DATASOURCE} \
@@ -111,36 +61,6 @@ asadmin --user=admin --passwordfile=/tmp/glassfishpwd \
                --connectionpoolid ${CONNECTION_POOL_NAME} \
                jdbc/olog
 
-# Configures security realm
-# Local LDAP
-# asadmin --user=admin --passwordfile=/tmp/glassfishpwd \
-#                create-auth-realm \
-#                --classname ${REALM_CLASS_NAME} \
-#                --property jaas-context=${REALM_JAAS_CTX}:base-dn=${REALM_BASE_DN}:directory=${REALM_URL}:search-filter=${REALM_SEARCH_FILTER}:group-base-dn=${REALM_GROUP_DN}:group-search-filter=${REALM_GROUP_FILTER}:assign-groups="olog-admins, olog-logbooks, olog-tags, olog-logs" \
-#                olog
-
-# LDAP settings 
-
-# LDAP_SETTINGS="jaas-context=${REALM_JAAS_CTX}:base-dn=${REALM_BASE_DN}:directory=${REALM_URL}:search-filter=${REALM_SEARCH_FILTER}"
-
-# if [ ! -z ${REALM_SEARCH_BIND_DN+x} ]; then
-#     LDAP_SETTINGS="${LDAP_SETTINGS}:search-bind-dn=${REALM_SEARCH_BIND_DN}"
-# fi
-
-# if [ ! -z  ${REALM_SEARCH_BIND_PASS+x} ]; then
-#     LDAP_SETTINGS="${LDAP_SETTINGS}:search-bind-password=${REALM_SEARCH_BIND_PASS}"
-# fi
-
-# if [ ! -z ${REALM_GROUP_DN+x} ]; then
-#     LDAP_SETTINGS="${LDAP_SETTINGS}:group-base-dn=${REALM_GROUP_DN}"
-# else
-#     LDAP_SETTINGS="${LDAP_SETTINGS}:assign-groups=\"olog-admins, olog-logbooks, olog-tags, olog-logs\""
-# fi
-
-# if [ ! -z ${REALM_GROUP_FILTER+x} ]; then
-#     LDAP_SETTINGS="${LDAP_SETTINGS}:group-search-filter=${REALM_GROUP_FILTER}"
-# fi
-
 set -x
 
 # Configure file-based authentication
@@ -151,26 +71,12 @@ asadmin --user=admin --passwordfile=/tmp/glassfishpwd \
         olog
 
 # See https://docs.oracle.com/cd/E18930_01/html/821-2433/create-file-user-1.html#SJSASEEREFMANcreate-file-user-1
-echo "AS_ADMIN_USERPASSWORD=1234" >> /tmp/glassfishpwd
+echo "AS_ADMIN_USERPASSWORD=${USER_PASSWORD}"          >> /tmp/glassfishpwd
 asadmin --user=admin --passwordfile=/tmp/glassfishpwd create-file-user --groups olog-admins olog-admin
 asadmin --user=admin --passwordfile=/tmp/glassfishpwd create-file-user --groups olog-logs olog-user
 
-echo "AS_ADMIN_PASSWORD=${ADMIN_PASSWORD}" > /tmp/glassfishpwd
+echo "AS_ADMIN_PASSWORD=${ADMIN_PASSWORD}"              > /tmp/glassfishpwd
 echo "AS_ADMIN_MASTERPASSWORD=${CERTIFICATE_PASSWORD}" >> /tmp/glassfishpwd
-
-# asadmin --user=admin --passwordfile=/tmp/glassfishpwd \
-#                  create-auth-realm \
-#                  --classname ${REALM_CLASS_NAME} \
-#                  --property "${LDAP_SETTINGS}" \
-#                  olog
-
-# Configures JNDI resource
-# asadmin --user=admin --passwordfile=/tmp/glassfishpwd \
-#                 create-custom-resource \
-#                 --restype ${JNDI_RESOURCE_TYPE} \
-#                 --factoryclass ${JNDI_FACTORY_CLASS} \
-#                 --property URL=${JNDI_URL}:javax.naming.security.principal=${JNDI_PRINCIPAL} \
-#                 ologGroups
 
 asadmin --user=admin --passwordfile=/tmp/glassfishpwd restart-domain
 
@@ -186,9 +92,6 @@ cp -r ${GLASSFISH_CONF_FOLDER}/logbook/Olog/public_html/* ${GLASSFISH_HOME}/glas
 # Changes web manager settings
 sed -i "s/allowDeletingLogs = false/allowDeletingLogs = true/" ${GLASSFISH_HOME}/glassfish/domains/domain1/applications/olog-service-${olog_version}/static/js/configuration.js
 sed -i "s/logId = \$log.attr('id');/logId = xml.log[0].id;/" ${GLASSFISH_HOME}/glassfish/domains/domain1/applications/olog-service-${olog_version}/static/js/rest.js
-# sed -i 's#datePickerDateFormatMometParseString = .*#datePickerDateFormatMometParseString = "DD/MM/YYYY hh:mm";#' ${GLASSFISH_HOME}/glassfish/domains/domain1/applications/olog-service-${olog_version}/static/js/configuration.js
-# sed -i 's#dateFormat = .*#dateFormat = "DD/MM/YY, hh:mm A";#' ${GLASSFISH_HOME}/glassfish/domains/domain1/applications/olog-service-${olog_version}/static/js/configuration.js
-# sed -i 's#datePickerDateFormat = .*#datePickerDateFormat = "dd/mm/yy";#' ${GLASSFISH_HOME}/glassfish/domains/domain1/applications/olog-service-${olog_version}/static/js/configuration.js
 
 sed -i 's;var serviceurl = window.location.protocol + "//" + window.location.host + "/Olog/resources/";var serviceurl = "https://localhost:8181/Olog/resources/";g' ${GLASSFISH_HOME}/glassfish/domains/domain1/applications/olog-service-${olog_version}/static/js/configuration.js
 
@@ -205,6 +108,11 @@ sed -i "s:s1as:olog:g" ${GLASSFISH_HOME}/glassfish/domains/domain1/config/domain
 
 cp ${GLASSFISH_CONF_FOLDER}/index.html ${GLASSFISH_HOME}/glassfish/domains/domain1/docroot/
 
-asadmin --user=admin --passwordfile=/tmp/glassfishpwd start-domain
+# This part should be done after the image is built:
 
+# Waits for the database to be ready
+# chmod +x /opt/wait-for-it/wait-for-it.sh
+# /opt/wait-for-it/wait-for-it.sh ${DB_MYSQL_URL}:3306
+
+# asadmin --user=admin --passwordfile=/tmp/glassfishpwd start-domain
 # rm -f /tmp/glassfishpwd
