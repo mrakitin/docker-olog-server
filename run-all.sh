a7a11b6aa60c2e8956b055b1366f2c5e99f9fbca
Maksim Rakitin <maxim.rakitin@gmail.com>
2018-11-28 08:29:56 -0500

    CLN: clean the run-all script from commented lines, which are not needed anymore

--- run-all.sh
+++ run-all.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-# docker network create --subnet=172.18.0.0/16 olog-network
-# mysql_ip=172.18.33.06
-
-# docker run -d --name=olog-mysql-db -e MYSQL_USER=olog_user -e MYSQL_ROOT_PASSWORD=password -e MYSQL_PASSWORD=password -e MYSQL_DATABASE=olog --network=olog-network --ip ${mysql_ip} mrakitin/olog-mysql-db:latest
 docker run -d --name=olog-mysql-db -e MYSQL_USER=olog_user -e MYSQL_ROOT_PASSWORD=password -e MYSQL_PASSWORD=password -e MYSQL_DATABASE=olog mrakitin/olog-mysql-db:latest
 
-# docker run -d --name=olog-server -p 4848:4848 -p 8181:8181 --network=olog-network --add-host olog-mysql-db:172.18.33.06 mrakitin/olog-server:latest asadmin --user=admin --passwordfile=/tmp/glassfishpwd start-domain -v
 docker run -d --name=olog-server -p 4848:4848 -p 8181:8181 --link olog-mysql-db mrakitin/olog-server:latest asadmin --user=admin --passwordfile=/tmp/glassfishpwd start-domain -v
 
